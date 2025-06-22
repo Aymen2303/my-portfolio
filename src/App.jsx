48d833a7b6a02d3265a41712f5d39a5763b0c9b1
Aymen2303 <aimenlalaibia0@gmail.com>
2025-06-22 23:56:11 +0100

    WIP, set up the project for future work

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello, I am Aimen!</h1>
+      <p>Software Engineer | Mobile Dev</p>
+    </div>
+  )
+}
+
+export default App
